6aec9ab4e8fd9d05be7ea2413f552fad2650c524
elfaquihiayoub <elfaquihi.ayoub@gmail.com>
2026-09-22 13:33:53 +0000

    adding tuto 8

--- Structurer l’application en composants/backend/categories.php
+++ Structurer l’application en composants/backend/categories.php
@@ -1,12 +1,13 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
+header('Content-Type: application/json; ');
+header('Access-Control-Allow-Origin: *');
 $categories=[
    [ "id" => 1,"nom" => "Développement"] ,
    [ "id" => 2,"nom" => "Design UI/UX"] ,
-   [ "id" => 3,"nom" => "Productivité"] ,
+   [ "id" => 3,"nom" => "Productivité"] 
 ];
 
 
- echo json_encode($categories);
+echo json_encode($categories);
 
 ?>
