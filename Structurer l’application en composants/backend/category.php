9e9808b9dc5cf36f795bcfd88c0e89719b267283
elfaquihiayoub <elfaquihi.ayoub@gmail.com>
2026-09-23 13:33:44 +0000

    adding changes about (Encapsuler les données d’un objet)

--- Structurer l’application en composants/backend/category.php
+++ Structurer l’application en composants/backend/category.php
@@ -2,19 +2,67 @@
 <?php 
 
 class category {
-    public $id;
-    public $nom;
-    public $couleur;
-    public $icone;
+    private int  $id;
+    private string $nom;
+    private string $couleur;
+    private string $icone;
 
-    public function __construct($id,$nom,$couleur,$icone)
+    public function __construct( int $id, string $nom, string $couleur, string $icone) 
     {
         $this->nom=$nom;
         $this->id=$id;
         $this->couleur=$couleur;
         $this->icone=$icone;
 
+    } 
+    // getters 
+     public function getId(): int {
+        return $this->id;
     }
+    public function getNom(): string {
+        return $this->nom;
+    }
+  
+    public function getCouleur(): string {
+        return $this->couleur;
+    }
+    public function getIcone(): string {
+        return $this->icone;
+    }
+
+    //  setters 
+
+    public function setNom(string $newName): void {
+        if(strlen($newName)>2){
+            $this->nom=$newName;
+        }
+        else{
+            echo "erreur name is too short";
+        }
+    }
+    public function setCouleur(string $newColor): void {
+        if(strtolower($newColor)!=strtolower($this->couleur)){
+            $this->couleur=$newColor;
+        }
+        else{
+            echo "vous avez choisr le meme couleur ";
+        }
+    }
+    public function setIcone(string $newIcone): void {
+        if(strtolower($newIcone)!=strtolower($this->icone)){
+            $this->icone=$newIcone;
+        }
+        else{
+            echo "vous avez choisr le meme icoone ";
+        }
+    }
+
+
+    
+
+
+
+
     public function afficher(){
         echo "le category est ".$this->nom." - ".$this->couleur." - ".$this->icone ;
     }
--- Structurer l’application en composants/backend/tester_object.php
+++ Structurer l’application en composants/backend/tester_object.php
@@ -3,7 +3,13 @@ require_once "category.php";
 
 
 $category=new category(1,"science","blue","Xjbd");
-$category->afficher();
+echo "le nom de category est : ".$category->getNom()."<br>";
+
+echo "le Couleur de category est : ".$category->getCouleur()."<br>";
+$category->setCouleur("red");
+echo "le Couleur de category apres le changment est  : ".$category->getCouleur();
+
+
 
 
 ?>
